c957779a533621306c2b671cf41d60766161822f
ALABENSALEMDBS <bensalem.ala@esprit.tn>
2025-08-25 21:00:40 +0100

    developpé home et contact footer et header

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './frontOffice/home/home.component';
+import { HeaderComponent } from './frontOffice/header/header.component';
+import { FooterComponent } from './frontOffice/footer/footer.component';
+import { ProjectsComponent } from './frontOffice/projects/projects.component';
+import { ContactComponent } from './frontOffice/contact/contact.component';
+import { AboutComponent } from './frontOffice/about/about.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+
+  {path:'home',component:HomeComponent},
+  {path:'footer',component:FooterComponent},
+  {path:'header',component:HeaderComponent},
+  {path:'projects',component:ProjectsComponent},
+  {path:'contact',component:ContactComponent},
+  {path:'about',component:AboutComponent},
+
+
+
+
+];
